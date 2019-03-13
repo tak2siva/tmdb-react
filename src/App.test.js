e948bf37778263fb272054c161d2dc4b9a8cad05
Shiva <tak2siva@gmail.com>
2019-03-13 15:57:10 +0530

    add test-1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import Featured from './Featured';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('<App />', () => {
+  it('Always renders Featured List', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find(<Featured/>)).to.have.lengthOf(1);
+  })
 });
