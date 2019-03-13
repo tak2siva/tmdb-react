87247d4592cd1ecb1609b63d0e6224e87638e122
Shiva <tak2siva@gmail.com>
2019-03-13 11:28:20 +0530

    extract featured component

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Card, Button, CardDeck, CardGroup, Row } from 'react-bootstrap';
-import { holder } from 'holderjs';
+import { Container, Row } from 'react-bootstrap';
+import Featured from './Featured'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      isLoaded: true,
-      movies: ["Captian Marvel", "Avengers Endgame", "Spider Man Far From Home"]
-    }
+    this.state = {}
   }
 
-  componentDidMount() {
-    fetch("/featured/movies")
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          isLoaded: true,
-          movies: res
-        }, err => {
-          if (err) {
-            console.log("Error fetching featured movies ", err)
-          }
-        })
-      });
-  }
-  
   render() {
-    const {isLoaded, movies} = this.state;
-
-    if (true) {
       return(
         <Container>
           <Row className="justify-content-md-center">
             <h3> tMDB </h3>
           </Row>
           <Row className="justify-content-md-center">
-        <CardGroup>
-        {movies.map(movie => {
-          return <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>{movie.name}</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-              content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>;
-        })}
-      </CardGroup>
+          <Featured/>
       </Row>
       </Container>
       )
-    }
-    return (
-      <div className="App">
-        <h3> Loading... </h3>
-      </div>
-    );
   }
 }
 
---
+++ src/Featured.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { holder } from 'holderjs';
+import { Card, CardGroup } from 'react-bootstrap';
+
+class Featured extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("/featured/movies")
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    isLoaded: true,
+                    movies: res
+                }, err => {
+                    if (err) {
+                        console.log("Error fetching featured movies ", err)
+                    }
+                })
+            });
+    }
+
+    render() {
+        const {isLoaded, movies} = this.state;
+        if (!isLoaded) {
+            return <div className="App">
+                <h3> Loading... </h3>
+          </div>
+        }
+        return (
+        <CardGroup>
+            {movies.map(movie => {
+            return <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+                <Card.Title>{movie.name}</Card.Title>
+                <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </Card>;
+            })}
+        </CardGroup>
+        );
+    }
+}
+
+export default Featured;
